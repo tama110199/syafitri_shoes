d33c6c10a9372b845563ed31df8f009c8d030cb5
Ramadhan Pratama <tama110199@gmail.com>
2020-10-23 11:30:36 +0700

    perbalikan issue prone

--- android/app/src/main/kotlin/com/reix19/syafitri_shoes/MainActivity.kt
+++ android/app/src/main/kotlin/com/reix19/syafitri_shoes/MainActivity.kt
@@ -5,8 +5,15 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+
+/**
+ * Main activity
+ * this class use to run flutter engine
+ * @constructor Create empty Main activity
+ */
+
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine)
-}
+    }
 }
